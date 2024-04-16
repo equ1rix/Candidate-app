dd25960f245c782872e67df1142ba851e73dca64
equ1rix <103569669+equ1rix@users.noreply.github.com>
2024-04-16 21:29:29 +0200

    task(src):Setup Firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 import reportWebVitals from './reportWebVitals';
+
+import App from './App';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCnUY41gfqoldcT2dov7dryeuX-nI5CcUU',
+  authDomain: 'candidate-app-4cbf7.firebaseapp.com',
+  databaseURL:
+    'https://candidate-app-4cbf7-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'candidate-app-4cbf7',
+  storageBucket: 'candidate-app-4cbf7.appspot.com',
+  messagingSenderId: '390865329830',
+  appId: '1:390865329830:web:6c5bc9f60c236f324b5e67',
+  measurementId: 'G-2E9XY1KHH7'
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
