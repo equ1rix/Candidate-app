5b8518069997d5a25cee76525d71a871f796b047
equ1rix <103569669+equ1rix@users.noreply.github.com>
2024-05-05 19:09:36 +0200

    fix(src): add .env file

--- src/components/Candidates/index.tsx
+++ src/components/Candidates/index.tsx
@@ -1,7 +1,8 @@
 import { Box } from '@mui/material';
+import { modalBG } from 'helpers/styles';
 
 const Candidates = () => (
-  <Box sx={{ bgcolor: '#116466', p: '10px', height: '100vh' }}></Box>
+  <Box sx={{ bgcolor: modalBG, p: '10px', height: '100vh' }}></Box>
 );
 
 export default Candidates;
