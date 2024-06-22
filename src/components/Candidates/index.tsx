7408466696308ae64034bd2a8c4844ddcd754182
equ1rix <nazar.pavlenko13@gmail.com>
2024-06-22 16:05:23 +0200

    task(candidates): add list pagination

--- src/components/Candidates/index.tsx
+++ src/components/Candidates/index.tsx
@@ -1,4 +1,6 @@
-import { Grid, Typography } from '@mui/material';
+import { useState } from 'react';
+import { Button, Grid, IconButton, Typography } from '@mui/material';
+import { ArrowBack, ArrowForward } from '@material-ui/icons';
 
 import { Candidate } from 'pages/Homepage';
 
@@ -7,6 +9,32 @@ type CandidatesProps = {
 };
 
 const Candidates = ({ candidatesToShow = [] }: CandidatesProps) => {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const itemPerPage = 12;
+
+  const totalPages = Math.ceil(candidatesToShow.length / itemPerPage);
+
+  const currentCandidates = candidatesToShow.slice(
+    (currentPage - 1) * itemPerPage,
+    currentPage * itemPerPage
+  );
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prev) => prev - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage((prev) => prev + 1);
+    }
+  };
+
   return (
     <Grid
       container
@@ -16,10 +44,11 @@ const Candidates = ({ candidatesToShow = [] }: CandidatesProps) => {
       className="bg-bg-modal"
       sx={{
         p: '10px',
-        height: { xs: 'calc(100vh - 56px)', sm: 'calc(100vh - 64px)' }
+        height: { xs: 'calc(100vh - 56px)', sm: 'calc(100vh - 64px)' },
+        position: 'relative'
       }}
     >
-      {candidatesToShow.map((candidate, index) => (
+      {currentCandidates.map((candidate) => (
         <Grid
           item
           key={candidate.id}
@@ -37,7 +66,6 @@ const Candidates = ({ candidatesToShow = [] }: CandidatesProps) => {
           }}
         >
           <Grid container alignItems="center">
-            <Typography sx={{ width: '15px', marginRight: '10px' }}>{index+1}</Typography>
             <Typography sx={{ width: '150px', marginRight: '10px' }}>
               {candidate.name}
             </Typography>
@@ -45,6 +73,38 @@ const Candidates = ({ candidatesToShow = [] }: CandidatesProps) => {
           </Grid>
         </Grid>
       ))}
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        sx={{
+          position: 'absolute',
+          bottom: '20px',
+          width: '400px',
+          left: 'calc(50% - 200px)'
+        }}
+      >
+        <IconButton onClick={handlePrevPage} disabled={currentPage === 1}>
+          <ArrowBack />
+        </IconButton>
+        {Array.from({ length: totalPages }, (_, index) => (
+          <Button
+            key={index}
+            color="inherit"
+            variant={currentPage === index + 1 ? 'contained' : 'outlined'}
+            onClick={() => handlePageChange(index + 1)}
+            sx={{ margin: '0px 5px' }}
+          >
+            {index + 1}
+          </Button>
+        ))}
+        <IconButton
+          onClick={handleNextPage}
+          disabled={currentPage === totalPages}
+        >
+          <ArrowForward />
+        </IconButton>
+      </Grid>
     </Grid>
   );
 };
