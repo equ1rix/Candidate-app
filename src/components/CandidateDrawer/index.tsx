d2aa7451b4f31cbe2368b70fc527dac48f0f2bfd
equ1rix <nazar.pavlenko13@gmail.com>
2024-08-07 21:47:49 +0200

    task(src): add comments feature

--- src/components/CandidateDrawer/index.tsx
+++ src/components/CandidateDrawer/index.tsx
@@ -1,31 +1,21 @@
 import { useTranslation } from 'react-i18next';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import {
   Box,
   Button,
-  Checkbox,
   Drawer,
-  FormControl,
-  FormControlLabel,
   Grid,
-  InputLabel,
-  MenuItem,
-  Select,
-  SelectChangeEvent,
   Tab,
   Tabs,
-  TextField,
   Typography
 } from '@mui/material';
-import { doc, updateDoc } from 'firebase/firestore';
 
-import { useFetchPositions } from 'hooks';
 import { mock } from 'helpers';
-import { db } from 'helpers/firebaseConfig';
 import { Candidate } from 'pages/Homepage';
 
 import CloseIcon from 'components/Icons/closeIcon';
-import Label from 'components/Label';
+import CandidateInfo from 'components/CandidateInfo';
+import CandidateComments from 'components/CandidateComments';
 
 type CandidateDrawerProps = {
   onClose: () => void;
@@ -36,96 +26,11 @@ const CandidateDrawer = ({
   onClose = mock,
   candidate
 }: CandidateDrawerProps) => {
-  const [name, setName] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [phoneNumber, setPhoneNumber] = useState<string>('');
-  const [gitHub, setGitHub] = useState<string>('');
-  const [linkedIn, setLinkedIn] = useState<string>('');
-  const [status, setStatus] = useState<boolean>(true);
-  const [isFavorite, setIsFavorite] = useState<boolean>(false);
   const [tab, setTab] = useState<number>(0);
-  const [position, setPosition] = useState('');
-
-  const { positions } = useFetchPositions();
   const { t } = useTranslation();
-
-  const arrayDataInput = [
-    { label: 'Name', name: 'name', value: name, func: setName },
-    { label: 'Email', name: 'email', value: email, func: setEmail },
-    { label: 'Phone', name: 'phone', value: phoneNumber, func: setPhoneNumber },
-    { label: 'GitHub', name: 'github', value: gitHub, func: setGitHub },
-    { label: 'LinkedIn', name: 'linkedin', value: linkedIn, func: setLinkedIn }
-  ];
-
-  const arrayDataCheckbox = [
-    {
-      label: 'Favorite',
-      value: isFavorite,
-      func: setIsFavorite
-    },
-    { label: 'Status', value: status, func: setStatus }
-  ];
-
-  const isButtonDisabled =
-    name === candidate?.name &&
-    email === candidate?.email &&
-    phoneNumber === candidate?.phone &&
-    gitHub === candidate?.github &&
-    linkedIn === candidate?.linkedin &&
-    status === candidate?.status &&
-    position === candidate?.position &&
-    isFavorite === candidate?.favorite;
-
-  useEffect(() => {
-    if (candidate) {
-      setName(candidate.name || '');
-      setEmail(candidate.email || '');
-      setPhoneNumber(candidate.phone || '');
-      setGitHub(candidate.github || '');
-      setLinkedIn(candidate.linkedin || '');
-      setStatus(candidate.status ?? true);
-      setIsFavorite(candidate.favorite ?? false);
-      setPosition(candidate.position || '');
-    }
-  }, [candidate]);
-
-  const updateCandidateInfo = async (updatedInfo: Partial<Candidate>) => {
-    if (candidate) {
-      const candidateDocRef = doc(db, 'candidates', candidate.id);
-      await updateDoc(candidateDocRef, updatedInfo);
-    }
-  };
 
   const handleChangeTab = (event: React.SyntheticEvent, newValue: number) => {
     setTab(newValue);
-  };
-
-  const handleChangePosition = (e: SelectChangeEvent) => {
-    setPosition(e.target.value);
-  };
-
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    stateHandler: React.Dispatch<React.SetStateAction<string>>
-  ) => stateHandler(e.target.value);
-
-  const handleChangeCheckbox = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    stateHandler: React.Dispatch<React.SetStateAction<boolean>>
-  ) => stateHandler(e.target.checked);
-
-  const handlerSave = () => {
-    updateCandidateInfo({
-      name: name,
-      email: email,
-      phone: phoneNumber,
-      github: gitHub,
-      linkedin: linkedIn,
-      status: status,
-      favorite: isFavorite,
-      position: position
-    });
-    onClose();
   };
 
   return (
@@ -168,69 +73,10 @@ const CandidateDrawer = ({
             <CloseIcon />
           </Button>
         </Grid>
-        {tab === 0 && (
-          <Box>
-            <Grid container direction="column" spacing={2}>
-              {arrayDataInput.map((el, index) => (
-                <Grid key={index} item xs={12}>
-                  <FormControl fullWidth>
-                    <TextField
-                      label={el.label}
-                      variant="outlined"
-                      name={el.name}
-                      value={el.value}
-                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                        handleChange(e, el.func)
-                      }
-                    />
-                  </FormControl>
-                </Grid>
-              ))}
-              {arrayDataCheckbox.map((el, index) => (
-                <Grid key={index} item xs={12}>
-                  <FormControlLabel
-                    control={
-                      <Checkbox
-                        checked={el.value}
-                        onChange={(e) => handleChangeCheckbox(e, el.func)}
-                      />
-                    }
-                    label={el.label}
-                  />
-                </Grid>
-              ))}
-              <Grid item>
-                <InputLabel id="position-select-label">Position</InputLabel>
-                <Select
-                  labelId="position-select-label"
-                  id="position-select"
-                  value={position}
-                  label="Position"
-                  onChange={handleChangePosition}
-                >
-                  {positions.map((pos) => (
-                    <MenuItem key={pos.id} value={pos.id}>
-                      {pos.position}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </Grid>
-              <Grid item>
-                <Box display="flex" justifyContent="flex-end">
-                  <Button
-                    onClick={handlerSave}
-                    disabled={isButtonDisabled}
-                    variant="contained"
-                    className="bg-bg-modalButton"
-                  >
-                    <Label label={t('add')} />
-                  </Button>
-                </Box>
-              </Grid>
-            </Grid>
-          </Box>
+        {tab === 0 && <CandidateInfo candidate={candidate} onClose={onClose} />}
+        {tab === 1 && candidate?.id != undefined && (
+          <CandidateComments candidateId={candidate?.id} />
         )}
-        {tab === 1 && <Box></Box>}
       </Box>
     </Drawer>
   );
---
+++ src/components/CandidateInfo/index.tsx
@@ -0,0 +1,181 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  TextField
+} from '@mui/material';
+import { doc, updateDoc } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { useFetchPositions } from 'hooks';
+import { mock } from 'helpers';
+import { db } from 'helpers/firebaseConfig';
+
+import { Candidate } from 'pages/Homepage';
+import Label from 'components/Label';
+
+type CandidateInfoProps = {
+  onClose: () => void;
+  candidate: Candidate | null;
+};
+
+const CandidateInfo = ({ onClose = mock, candidate }: CandidateInfoProps) => {
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [phoneNumber, setPhoneNumber] = useState<string>('');
+  const [gitHub, setGitHub] = useState<string>('');
+  const [linkedIn, setLinkedIn] = useState<string>('');
+  const [status, setStatus] = useState<boolean>(true);
+  const [isFavorite, setIsFavorite] = useState<boolean>(false);
+  const [position, setPosition] = useState('');
+
+  const { positions } = useFetchPositions();
+  const { t } = useTranslation();
+
+  const arrayDataInput = [
+    { label: 'Name', name: 'name', value: name, func: setName },
+    { label: 'Email', name: 'email', value: email, func: setEmail },
+    { label: 'Phone', name: 'phone', value: phoneNumber, func: setPhoneNumber },
+    { label: 'GitHub', name: 'github', value: gitHub, func: setGitHub },
+    { label: 'LinkedIn', name: 'linkedin', value: linkedIn, func: setLinkedIn }
+  ];
+
+  const arrayDataCheckbox = [
+    {
+      label: 'Favorite',
+      value: isFavorite,
+      func: setIsFavorite
+    },
+    { label: 'Status', value: status, func: setStatus }
+  ];
+
+  const isButtonDisabled =
+    name === candidate?.name &&
+    email === candidate?.email &&
+    phoneNumber === candidate?.phone &&
+    gitHub === candidate?.github &&
+    linkedIn === candidate?.linkedin &&
+    status === candidate?.status &&
+    position === candidate?.position &&
+    isFavorite === candidate?.favorite;
+
+  useEffect(() => {
+    if (candidate) {
+      setName(candidate.name || '');
+      setEmail(candidate.email || '');
+      setPhoneNumber(candidate.phone || '');
+      setGitHub(candidate.github || '');
+      setLinkedIn(candidate.linkedin || '');
+      setStatus(candidate.status ?? true);
+      setIsFavorite(candidate.favorite ?? false);
+      setPosition(candidate.position || '');
+    }
+  }, [candidate]);
+
+  const updateCandidateInfo = async (updatedInfo: Partial<Candidate>) => {
+    if (candidate) {
+      const candidateDocRef = doc(db, 'candidates', candidate.id);
+      await updateDoc(candidateDocRef, updatedInfo);
+    }
+  };
+
+  const handleChangePosition = (e: SelectChangeEvent) => {
+    setPosition(e.target.value);
+  };
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    stateHandler: React.Dispatch<React.SetStateAction<string>>
+  ) => stateHandler(e.target.value);
+
+  const handleChangeCheckbox = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    stateHandler: React.Dispatch<React.SetStateAction<boolean>>
+  ) => stateHandler(e.target.checked);
+
+  const handlerSave = () => {
+    updateCandidateInfo({
+      name: name,
+      email: email,
+      phone: phoneNumber,
+      github: gitHub,
+      linkedin: linkedIn,
+      status: status,
+      favorite: isFavorite,
+      position: position
+    });
+    onClose();
+  };
+  return (
+    <Box>
+      <Grid container direction="column" spacing={2}>
+        {arrayDataInput.map((el, index) => (
+          <Grid key={index} item xs={12}>
+            <FormControl fullWidth>
+              <TextField
+                label={el.label}
+                variant="outlined"
+                name={el.name}
+                value={el.value}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  handleChange(e, el.func)
+                }
+              />
+            </FormControl>
+          </Grid>
+        ))}
+        {arrayDataCheckbox.map((el, index) => (
+          <Grid key={index} item xs={12}>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={el.value}
+                  onChange={(e) => handleChangeCheckbox(e, el.func)}
+                />
+              }
+              label={el.label}
+            />
+          </Grid>
+        ))}
+        <Grid item>
+          <InputLabel id="position-select-label">Position</InputLabel>
+          <Select
+            labelId="position-select-label"
+            id="position-select"
+            value={position}
+            label="Position"
+            onChange={handleChangePosition}
+          >
+            {positions.map((pos) => (
+              <MenuItem key={pos.id} value={pos.id}>
+                {pos.position}
+              </MenuItem>
+            ))}
+          </Select>
+        </Grid>
+        <Grid item>
+          <Box display="flex" justifyContent="flex-end">
+            <Button
+              onClick={handlerSave}
+              disabled={isButtonDisabled}
+              variant="contained"
+              className="bg-bg-modalButton"
+            >
+              <Label label={t('add')} />
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default CandidateInfo;
